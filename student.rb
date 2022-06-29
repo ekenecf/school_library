d2391b2a81880cd5c038d38fbdaa640899d5151f
ekenecf <ekens2u@gmail.com>
2022-06-29 17:01:19 +0100

    Remove Unused code from code base

--- student.rb
+++ student.rb
@@ -11,32 +11,3 @@ class Student < Person
     "¯\(ツ)/¯"
   end
 end
-
-# class Animal
-#     def initialize(type, number_of_legs, name = "Unknown")
-#       @id = Random.rand(1..1000)
-#       @name = name
-#       @number_of_legs = number_of_legs
-#       @type = type
-#     end
-
-#     attr_accessor :type, :number_of_legs, :name
-
-#     def remove_leg
-#         remover = Remover.new()
-#         remover.decrease(@number_of_legs)
-#     end
-# end
-
-#   class Remover
-#     def decrease(number, step = 1)
-#       number -= step
-#     end
-# end
-# animal = Animal.new("lion", 4, "Rex")
-#  animal.number_of_legs = 5
-#  p animal.number_of_legs
-# p animal.number_of_legs
-# p animal.remove_leg()
-# p animal.number_of_legs
-# p ["insect", "bug"].member?('food')
