14120b241c0c8bf0eae29b24024a4be905aeab05
ekenecf <ekens2u@gmail.com>
2022-07-02 15:05:43 +0100

    set up associations

--- example.rb
+++ example.rb
@@ -1,11 +1,55 @@
 require_relative './trimmer_decorator'
 require_relative './person'
 require_relative './capitalize_decorator'
+require_relative 'base_decorator'
 
 person = Person.new(22, 'maximilianus')
-p person.correct_name
+person.correct_name
 capitalizedperson = CapitalizeDecorator.new(person)
-p capitalizedperson.correct_name
+capitalizedperson.correct_name
 capitalizedtrimmedperson = TrimmerDecorator.new(person)
-p capitalizedtrimmedperson
-p capitalizedtrimmedperson.correct_name
+capitalizedtrimmedperson.correct_name
+base = BaseDecorator.new(person)
+base.correct_name
+
+# class Owner
+#     attr_accessor :name
+#     attr_reader :animals
+
+#     def initialize(name)
+#       @name = name
+#       @animals = []
+#     end
+
+#     # Instead of setter for entire collection a method to add animals one by one
+#     def add_animal(animal)
+#       @animals.push(animal)
+#       animal.owner = self
+#     end
+# end
+
+#   class Animal
+#     attr_accessor :owner
+#     def initialize(type, number_of_legs, name = "Unknown")
+#       @id = Random.rand(1..1000)
+#       @name = name
+#       @number_of_legs = number_of_legs
+#       @type = type
+#     end
+
+#     def owner=(owner)
+#         @owner = owner
+#         owner.animals.push(self) unless owner.animals.include?(self)
+#     end
+#   end
+
+#   class Dog < Animal
+#     def initialize(color, name = "Unknown")
+#       super("dog", 4, name)
+#       @color = color
+#     end
+
+#     def bring_a_stick
+#       "Here is your stick: ---------"
+#     end
+#   end
---
+++ rental.rb
@@ -0,0 +1,27 @@
+require_relative './book'
+
+class Rental
+  attr_accessor :date
+  attr_reader :book, :person
+
+  def initialize(date, person, book)
+    @rentals = []
+    @date = date
+
+    @person = person
+    @person.rentals.push(self) unless @person.rentals.include?(self)
+
+    @book = book
+    @book.rentals.push(self) unless @book.rentals.include?(self)
+  end
+
+  def person=(person)
+    @person = person
+    @person.rentals.push(self) unless @person.rentals.include?(self)
+  end
+
+  def book=(book)
+    @book = book
+    @book.rentals.push(self) unless @book.rentals.include?(self)
+  end
+end
