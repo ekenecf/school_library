d9fc1b4cc2e2168941324db742986a585ac21829
ekenecf <ekens2u@gmail.com>
2022-07-06 14:06:15 +0100

    edit app.rb file

--- app.rb
+++ app.rb
@@ -4,7 +4,6 @@ require_relative './student'
 require_relative './person'
 require_relative './rental'
 require_relative './display'
-require_relative './list_functions'
 require_relative './create_book_rental'
 require_relative 'create_person'
 
