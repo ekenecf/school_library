de07ae43bcbb34ecfd861c30ed08fd3be7d1835a
lyamz <davidlyangenda@gmail.com>
2022-07-08 16:19:16 +0200

    test the book class

---
+++ spec/book_spec.rb
@@ -0,0 +1,15 @@
+
+require './book'
+
+describe Book do
+  context 'testing Book class' do
+    book = Book.new(title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', id: 1)
+    it 'Book title should return The Great Gatsby' do
+      expect(book.title).to eq 'The Great Gatsby'
+    end
+
+    it 'Book author should return F. Scott Fitzgerald' do
+      expect(book.author).to eq 'F. Scott Fitzgerald'
+    end
+  end
+end
