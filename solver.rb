bb6643fb5914213323517621ee3bf9c3161aa83b
lyamz <davidlyangenda@gmail.com>
2022-07-09 01:07:58 +0200

    Create a method called fizzbuzz that takes one argument, an integer N, and returns a string.

--- solver.rb
+++ solver.rb
@@ -9,3 +9,17 @@ class Solver
     def reverse(word)
         word.reverse
       end
+
+      def fizz_buzz(number)
+        if (number % 3).zero? and (number % 5).zero?
+          'fizzbuzz'
+        elsif (number % 3).zero?
+          'fizz'
+        elsif (number % 5).zero?
+          'buzz'
+        else
+          number.to_s
+        end
+      end
+    end
+    
